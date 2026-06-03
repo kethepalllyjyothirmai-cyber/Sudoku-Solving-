d84cd7b3d505543ed833e2a003ff728dc95aa688
kethepalllyjyothirmai-cyber <kethepalllyjyothirmai@gmail.com>
2026-06-03 13:22:46 +0530

    Sudoku Program .py

---
+++ Sudoku Program .py
@@ -0,0 +1,75 @@
+def print_board(board):
+    for row in board:
+        print(row)
+
+def find_empty(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return (i, j)
+    return None
+
+def is_valid(board, num, pos):
+    row, col = pos
+
+    # Check row
+    for j in range(9):
+        if board[row][j] == num and col != j:
+            return False
+
+    # Check column
+    for i in range(9):
+        if board[i][col] == num and row != i:
+            return False
+
+    # Check 3x3 box
+    box_x = col // 3
+    box_y = row // 3
+
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if board[i][j] == num and (i, j) != pos:
+                return False
+
+    return True
+
+def solve(board):
+    empty = find_empty(board)
+
+    if not empty:
+        return True
+
+    row, col = empty
+
+    for num in range(1, 10):
+        if is_valid(board, num, (row, col)):
+            board[row][col] = num
+
+            if solve(board):
+                return True
+
+            board[row][col] = 0
+
+    return False
+
+# Example Sudoku Puzzle
+board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+print("Sudoku Puzzle:")
+print_board(board)
+
+if solve(board):
+    print("\nSolved Sudoku:")
+    print_board(board)
+else:
+    print("No solution exists.")
